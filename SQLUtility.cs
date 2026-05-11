bda25a6d0e56ad29a008fa3edf3a2d39603e91c9
Hanoch Padwa <hanochpadwa37@gmail.com>
2026-05-11 18:12:00 +0100

    cleanup

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -22,14 +22,45 @@ namespace CPUFramework
             cmd.CommandText = sqlstatement;
             var dr = cmd.ExecuteReader();
             dt.Load(dr);
+            SetAllColumnsAllowNull(dt);
             return dt;
+        }
+
+        public static void ExecuteSql(string sqlstatement)
+        {
+            GetDataTable(sqlstatement);
+        }
+
+        public static int GetFirstcolumnFirstRowValue(string sql)
+        {
+            int n = 0;
+
+            DataTable dt = GetDataTable(sql);
+            if (dt.Rows.Count > 0 && dt.Columns.Count > 0)
+            {
+                if (dt.Rows[0][0] != DBNull.Value)
+                {
+                    
+                    int.TryParse(dt.Rows[0][0].ToString(), out n);
+                }
+
+            }
+
+            return n;
+        }
+        private static void SetAllColumnsAllowNull(DataTable dt)
+        {
+            foreach (DataColumn c in dt.Columns)
+            {
+                c.AllowDBNull = true;
+            }
         }
 
         public static void DebugPrintDataTable(DataTable dt)
         {
-            foreach(DataRow r in dt.Rows)
+            foreach (DataRow r in dt.Rows)
             {
-                foreach(DataColumn c in dt.Columns)
+                foreach (DataColumn c in dt.Columns)
                 {
                     Debug.Print(c.ColumnName + " = " + r[c.ColumnName].ToString());
                 }
